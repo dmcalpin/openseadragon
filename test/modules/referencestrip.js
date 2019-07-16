948c39d037e00c705cca8dd5c118e4f9517cf20f
David McAlpine <dave@mcalpine.io>
2019-07-15 22:27:03 -0400

    allow reference strip to work in a web component

--- test/modules/referencestrip.js
+++ test/modules/referencestrip.js
@@ -44,4 +44,33 @@
         done();
     });
 
+    // ----------
+    QUnit.test('shadow dom', function(assert) {
+        var done = assert.async();
+
+        class ShadowElem extends HTMLElement {
+            constructor() {
+                super();
+                this.attachShadow({ mode: 'open' });
+                this.shadowRoot.innerHTML = `<div id="shadow-example"></div>`
+
+                createViewer({
+                    element: this.shadowRoot.querySelector('#shadow-example'),
+                    sequenceMode: true,
+                    showReferenceStrip: true,
+                    tileSources: [
+                        '/test/data/tall.dzi',
+                        '/test/data/wide.dzi',
+                    ]
+                });
+            }
+        }
+        window.customElements.define('shadow-elem', ShadowElem);
+
+        $('<shadow-elem></shadow-elem>').appendTo("#qunit-fixture");
+
+        assert.ok(viewer.referenceStrip, 'referenceStrip exists');
+        done();
+    })
+
 })();
